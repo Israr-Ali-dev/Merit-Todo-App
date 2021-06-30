84bc4b42e4b70cc02d13ac90c441ca4e94193b9e
israr <israrmughal214@gmail.com>
2021-06-30 20:28:08 +0500

    Frontend complete with Responsive layout and complete working TODOs App

---
+++ frontend/src/components/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import ToDoItem from './ToDoItems';
+import { createList } from '../apiCore';
+
+function App() {
+  const [task, setTask] = useState('');
+  const [items, setItems] = useState([]);
+
+  // Input Value Change Handler
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setTask(newValue);
+  }
+
+  // Creating Todo Task With Backend API
+  function addTask(list) {
+    console.log(task);
+    createList(task).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.data);
+        setItems((prevValues) => {
+          return [...prevValues, data.data];
+        });
+        setTask('');
+      }
+    });
+  }
+
+  return (
+    <div className='container'>
+      <div className='heading'>
+        <h1>Merit ToDo App</h1>
+      </div>
+      <div className='form'>
+        <input
+          name='taskInput'
+          type='text'
+          onChange={handleChange}
+          value={task}
+        />
+        <button
+          onClick={(e) => {
+            addTask(e.target.value);
+          }}>
+          <span>ADD</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((item, index) => (
+            <ToDoItem key={index} text={item} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
